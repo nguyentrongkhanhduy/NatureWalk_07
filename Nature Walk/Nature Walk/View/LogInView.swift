909b1ee9429c66743b8e8396bde3a5a99316cd37
nguyentrongkhanhduy <nguyentrongkhanhduy@gmail.com>
2025-02-10 18:40:14 -0500

    Revert "Remove code for work division"
    This reverts commit 99a78c8be0b599deae1592491a1c69b877de6fb6.

--- Nature Walk/Nature Walk/View/LogInView.swift
+++ Nature Walk/Nature Walk/View/LogInView.swift
@@ -26,7 +26,90 @@ struct LogInView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Login view")
+                Text("Nature Walk")
+                    .font(Font.custom("DancingScript-Bold", size: 50))
+                    .foregroundColor(Color("bigTextColor"))
+                    .padding(.top, 90)
+                    .padding(.bottom, 60)
+                
+                
+                Text("Welcome back!")
+                    .font(Font.custom("DancingScript-Regular", size: 20))
+                    .foregroundColor(Color("bigTextColor"))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 20)
+                
+                VStack {
+                    CustomizedTextField(value: $username, label: "Email", isSecure: false)
+                        .padding(.bottom, 15)
+                    
+                    CustomizedTextField(
+                        value: $password,
+                        label: "Password",
+                        isSecure: true
+                    )
+                }
+                .padding()
+                .background(content: {
+                    Rectangle()
+                        .fill(Color(white: 1, opacity: 0.4))
+                        .cornerRadius(10)
+                })
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color("strokeColor"), lineWidth: 2)
+                )
+                .padding(.horizontal)
+                
+                RememberMeButton(rememberMe: $isRemember)
+                    .padding(.leading, 20)
+                    .padding(.top, 10)
+                
+                Button {
+                    if username.isEmpty || password.isEmpty {
+                        showAlert = true
+                    } else {
+                        if !isValidEmail(email: username) {
+                            showEmailAlert = true
+                        } else {
+                            user.email = username
+                            user.password = password
+                            toMainView = true
+                            if isRemember {
+                                UserDefaults.standard.set(true, forKey: "isRememberUser")
+                            }
+                        }
+                    }
+                } label: {
+                    Text("Login")
+                        .padding()
+                        .foregroundColor(.white)
+                        .font(Font.custom("Exo2-Bold", size: 20))
+                        .frame(maxWidth: .infinity)
+                        .background(
+                            Rectangle()
+                                .fill(Color("bigTextColor"))
+                                .cornerRadius(10)
+                        )
+                        .padding()
+                }
+                .padding(.top, 25)
+                .alert("Missing value", isPresented: $showAlert) {
+                    Button("OK", role: .cancel) { }
+                } message: {
+                    Text("Please fill all fields.")
+                }
+                .alert(
+                    "Invalid email",
+                    isPresented: $showEmailAlert) {
+                        Button("OK", role: .cancel) { }
+                    } message: {
+                        Text("Please enter a correct email address.")
+                    }
+
+                
+                
+                Spacer()
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(

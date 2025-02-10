ef33b6b868ecc35f4c42914ea21d3bac80681224
nguyentrongkhanhduy <nguyentrongkhanhduy@gmail.com>
2025-02-10 18:42:35 -0500

    Code revert for work division
    - ListNatureView
    - LogInView

--- Nature Walk/Nature Walk/View/ListNatureView.swift
+++ Nature Walk/Nature Walk/View/ListNatureView.swift
@@ -59,86 +59,8 @@ struct ListNatureView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                VStack(alignment: .leading){
-                    Text("Hi, \(user.username)")
-                    Text("Welcome to Nature Walk!")
-                }
-                .font(Font.custom("DancingScript-Bold", size: 35))
-                .foregroundColor(Color("bigTextColor"))
-                .padding(.vertical, 20)
-                
-                VStack {
-                    HStack {
-                        Text("Let's begin your adventure!")
-                            .font(Font.custom("Exo2-Bold", size: 16))
-                            .foregroundColor(Color("bigTextColor"))
-                        Spacer()
-                        Menu {
-                            Picker(selection: $sortBy) {
-                                Text("Default").tag("none")
-                                Text("Price Low-High").tag("priceIncrease")
-                                Text("Price High-Low").tag("priceDecrease")
-                                Text("Rating Low-High").tag("ratingIncrease")
-                                Text("Rating High-Low").tag("ratingDecrease")
-                                
-                            } label: {}
-                        } label: {
-                            Label(label, systemImage: "arrow.up.arrow.down")
-                                .font(Font.custom("Exo2-Regular", size: 14))
-                                .foregroundColor(Color("rowColor"))
-                                .padding(5)
-                                .background(content: {
-                                    Rectangle()
-                                        .fill(Color(white: 1, opacity: 0.4))
-                                        .cornerRadius(10)
-                                })
-                                .overlay {
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color("strokeColor"), lineWidth: 2)
-                                }
-                        }
-                        
-                        
-                    }
-                    .padding(.horizontal)
-                    .padding(.top)
-                    
-                    List {
-                        ForEach(sortedList) { natureWalk in
-                            NavigationLink {
-                                DetailView(natureWalk: natureWalk)
-                            } label: {
-                                RowView(
-                                    natureWalk: natureWalk
-                                ) { natureWalk in
-                                    
-                                }
-                            }
-                            
-                        }
-                        .listRowSeparator(.hidden)
-                        .listRowBackground(
-                            Rectangle()
-                                .fill(Color("rowColor"))
-                                .cornerRadius(10)
-                                .padding(3)
-                        )
-                    }
-                    .padding(8)
-                    .animation(.default, value: sortedList)
-                    .listStyle(.plain)
-                }
-                
-                .background(content: {
-                    Rectangle()
-                        .fill(Color(white: 1, opacity: 0.4))
-                        .cornerRadius(10)
-                })
-                .overlay {
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color("strokeColor"), lineWidth: 2)
-                }
-                .padding(.horizontal, 8)
+                Text("List view")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
             .background(
                 Color(Color("backgroundColor"))
